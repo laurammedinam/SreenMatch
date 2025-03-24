22977a3bae8b6cf32bfeac209073cd54b772f618
Laura Medina <laurammedinam@gmail.com>
2025-03-24 17:14:54 -0500

    new files

---
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+    private String titulo;
+    private Integer totalTemporadas,
+    private Double evaluacion,
+    private Categoria genro;
+    private String poster,
+    private String genero,
+    private String actores,
+    private String sinopsis) {
+    }
+
+}
